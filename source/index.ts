9cd97d3a7488eeab32580e592a8a1f6d08cea72a
Ziphil <yfe23457@nifty.com>
2025-04-09 21:23:08 +0900

    複数言語の出力ができるよう変更

--- source/index.ts
+++ source/index.ts
@@ -17,6 +17,7 @@ type ExampleSpec = {
   translation: string
 };
 type ExampleIndex = `${number}` | "last" | "all";
+type Language = "shaleian" | "fennese";
 type ImageMode = "landscape" | "portrait";
 
 function resolveIndices(indices: ExampleIndex | Array<ExampleIndex>, count: number): Array<number> {
@@ -33,73 +34,75 @@ function resolveIndices(indices: ExampleIndex | Array<ExampleIndex>, count: numb
   return resolvedIndices;
 }
 
-async function loadExampleSpecs(): Promise<Array<ExampleSpec>> {
-  const yamlSpecs = await fs.readFile("source/data/data.yaml", {encoding: "utf-8"});
+async function loadExampleSpecs(language: Language): Promise<Array<ExampleSpec>> {
+  const yamlSpecs = await fs.readFile(`source/data/${language}.yaml`, {encoding: "utf-8"});
   const specs = yaml.load(yamlSpecs) as any;
   return specs;
 }
 
-async function generatePages(specs: Array<ExampleSpec>, mode: ImageMode, indices: Array<number>): Promise<void> {
+async function generatePages(specs: Array<ExampleSpec>, language: Language, mode: ImageMode, indices: Array<number>): Promise<void> {
   const template = await fs.readFile(`source/page/${mode}/template.html`, {encoding: "utf-8"});
   await Promise.all(indices.map(async (index) => {
-    const output = dotjs.template(template)({...specs[index], number: index + 1});
-    await fs.mkdir(`out/${mode}/page`, {recursive: true});
-    await fs.writeFile(`out/${mode}/page/${index + 1}.html`, output);
+    const output = dotjs.template(template)({...specs[index], number: index + 1, language});
+    await fs.mkdir(`out/${language}/${mode}/page`, {recursive: true});
+    await fs.writeFile(`out/${language}/${mode}/page/${index + 1}.html`, output);
   }));
 }
 
-async function generateStyle(mode: ImageMode): Promise<void> {
+async function generateStyle(language: Language, mode: ImageMode): Promise<void> {
   const result = await sass.compileAsync(`source/page/${mode}/style.scss`);
-  await fs.mkdir(`out/${mode}/page`, {recursive: true});
-  await fs.writeFile(`out/${mode}/page/style.css`, result.css);
+  await fs.mkdir(`out/${language}/${mode}/page`, {recursive: true});
+  await fs.writeFile(`out/${language}/${mode}/page/style.css`, result.css);
 }
 
-async function generateImages(mode: ImageMode, indices: Array<number>): Promise<void> {
+async function generateImages(language: Language, mode: ImageMode, indices: Array<number>): Promise<void> {
   const viewport = (mode === "landscape") ? {width: 1920, height: 1080} : {width: 1080, height: 1920};
   const browser = await puppeteer.launch({defaultViewport: viewport});
   await Promise.all(indices.map(async (index) => {
     const page = await browser.newPage();
-    await page.goto(`file://${process.cwd()}/out/${mode}/page/${index + 1}.html`);
+    await page.goto(`file://${process.cwd()}/out/${language}/${mode}/page/${index + 1}.html`);
     await page.waitForSelector("#main");
     const element = await page.$("#main");
     if (element !== null) {
-      await fs.mkdir(`out/${mode}/image`, {recursive: true});
-      await element.screenshot({path: `out/${mode}/image/${index + 1}.png`});
+      await fs.mkdir(`out/${language}/${mode}/image`, {recursive: true});
+      await element.screenshot({path: `out/${language}/${mode}/image/${index + 1}.png`});
     }
     await page.close();
   }));
   await browser.close();
 }
 
-async function generateCompressedImages(mode: ImageMode, indices: Array<number>): Promise<void> {
-  const specs = await loadExampleSpecs();
+async function generateCompressedImages(language: Language, mode: ImageMode, indices: Array<number>): Promise<void> {
+  const specs = await loadExampleSpecs(language);
   await Promise.all(indices.map(async (index) => {
-    const buffer = await fs.readFile(`out/${mode}/image/${index + 1}.png`);
+    const buffer = await fs.readFile(`out/${language}/${mode}/image/${index + 1}.png`);
     const convertedBuffer = await pngToJpeg({quality: 100})(buffer) as any;
-    await fs.writeFile(`out/${mode}/image/${index + 1}-comp.jpg`, convertedBuffer);
+    await fs.writeFile(`out/${language}/${mode}/image/${index + 1}-comp.jpg`, convertedBuffer);
   }));
 }
 
-async function generate(mode: ImageMode, indices: ExampleIndex | Array<ExampleIndex>): Promise<void> {
-  const specs = await loadExampleSpecs();
+async function generate(language: Language, mode: ImageMode, indices: ExampleIndex | Array<ExampleIndex>): Promise<void> {
+  const specs = await loadExampleSpecs(language);
   const resolvedIndices = resolveIndices(indices, specs.length);
   await Promise.all([
-    generatePages(specs, mode, resolvedIndices),
-    generateStyle(mode)
+    generatePages(specs, language, mode, resolvedIndices),
+    generateStyle(language, mode)
   ]);
-  await generateImages(mode, resolvedIndices);
-  await generateCompressedImages(mode, resolvedIndices);
+  await generateImages(language, mode, resolvedIndices);
+  await generateCompressedImages(language, mode, resolvedIndices);
   console.log("done");
 }
 
 async function run(): Promise<void> {
   const options = commandLineArgs([
     {name: "index", alias: "i", multiple: true, defaultOption: true},
+    {name: "language", alias: "l", type: String, defaultValue: "shaleian"},
     {name: "mode", alias: "m"}
   ]);
+  const language = options.language as Language;
   const mode = options.mode as ImageMode;
   const indices = options.index as Array<ExampleIndex>;
-  await generate(mode, indices);
+  await generate(language, mode, indices);
 }
 
 run().catch((error) => console.error(error));
